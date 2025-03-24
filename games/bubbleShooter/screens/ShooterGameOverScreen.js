f3dfc665485b3eb9d7785c93ce82f0bc4c414234
lohielisabeth <ellipetti01@hotmail.com>
2025-03-24 12:01:00 +0200

    Lisätty GameOver screen ja pallojen random pastel colorit bubbleShooteriin

---
+++ games/bubbleShooter/screens/ShooterGameOverScreen.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const ShooterGameOver = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Game Over</Text>
+      <View style={styles.buttonContainer}>
+        <Button 
+          title="Play Again"
+          onPress={() => navigation.replace('BubbleShooter')}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button 
+          title="Back to Menu"
+          onPress={() => navigation.navigate('Home')}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#222',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 36,
+    color: '#fff',
+    marginBottom: 40,
+  },
+  buttonContainer: {
+    marginVertical: 10,
+    width: 200,
+  }
+});
+
+export default ShooterGameOver;
