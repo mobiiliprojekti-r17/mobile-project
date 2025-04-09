41b2e1ce3bc33cba11e2230eb425458070bc6cf2
Kekevaa <155944877+Kekevaa@users.noreply.github.com>
2025-04-09 13:36:04 +0300

    Revert "FlappyBird aloittelu"

--- games/FlappyBird/components/Bird.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { View } from 'react-native';
-import birdStyles from '../styles/birdStyles';
-
-const Bird = ({ birdBottom }) => {
-  return <View style={[birdStyles.bird, { bottom: birdBottom }]} />;
-};
-
-export default Bird;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,7 +5,6 @@ import styles from "../styles/HomeScreenStyles";
 import { useNickname } from '../context/context';
 import { useFonts, PressStart2P_400Regular } from '@expo-google-fonts/press-start-2p';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import FlappyBird from '../games/FlappyBird/screens/FlappyBirdScreen';
 
 const HomeScreen = ({ navigation }) => {
   const { nickname, setNickname } = useNickname();
@@ -47,20 +46,24 @@ const HomeScreen = ({ navigation }) => {
 
   const startGame = (game, difficulty) => {
     if (game === 'Sudoku') {
+      // Navigoidaan Sudoku-näyttöön ja annetaan vaikeustaso
       navigation.navigate('Sudoku', { nickname, difficulty });
     } else if (game === 'Minesweeper') {
+      // Navigoidaan Minesweeper-näyttöön ja annetaan vaikeustaso
       navigation.navigate('Minesweeper', { nickname, difficulty });
     } else {
+      // Muut pelit käynnistetään oletusvaikeudella
       navigation.navigate(game, { nickname });
     }
   };
 
   const checkNicknameAndProceed = (game) => {
     if (!nickname.trim()) {
-  
+      // Jos nimimerkki puuttuu, näytetään virhe
       showModal('Warning', 'Please enter a nickname first!', 'error');
     } else {
-      setSelectedGame(game);  
+      setSelectedGame(game);  // Asetetaan valittu peli
+      // Pelin käynnistäminen heti valinnan yhteydessä
       if (game === 'Sudoku' || game === 'Minesweeper') {
         setRestartModalVisible(true);
         setModalMessage('Choose difficulty');
@@ -81,10 +84,10 @@ const HomeScreen = ({ navigation }) => {
   const startMinesweeper = () => {
     checkNicknameAndProceed('Minesweeper');
   };
-const startFlappyBird = () => checkNicknameAndProceed ('FlappyBird');
 
   const handleDifficultyChange = (level) => {
-    startGame(selectedGame, level); 
+    // Käynnistetään peli valitulla vaikeustasolla
+    startGame(selectedGame, level);  // Käynnistetään valittu peli ja annetaan vaikeustaso
     setRestartModalVisible(false);
   };
 
@@ -137,10 +140,6 @@ const startFlappyBird = () => checkNicknameAndProceed ('FlappyBird');
               <TouchableOpacity style={styles.MinesweeperButton} onPress={startMinesweeper}>
                 <Text style={styles.gameButtonText}>Minesweeper</Text>
                 <Icon name="bomb" size={30} color="white" style={{ marginTop: 5 }} />
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.FlappyBirdButton} onPress={startFlappyBird}>
-                <Text style={styles.gameButtonText}>FlappyBird</Text>
-                <Icon name="twitter" size={30} color="white" style={{ marginTop: 5 }} />
               </TouchableOpacity>
             </View>
           </View>
