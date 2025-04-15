3b13ecb8322e02c699a4fbcc2739459aea52a2b6
lohielisabeth <ellipetti01@hotmail.com>
2025-04-15 13:56:27 +0300

    palloihin pop efekti

--- games/bubbleShooter/styles/shooterStyles.js
+++ games/bubbleShooter/styles/shooterStyles.js
@@ -1,3 +1,4 @@
+// shooterStyles.js
 import { StyleSheet } from 'react-native';
 
 const shooterStyles = StyleSheet.create({
@@ -11,50 +12,61 @@ const shooterStyles = StyleSheet.create({
     elevation: 10,
     justifyContent: 'center',
     alignItems: 'center',
-     marginTop:40
+    marginTop: 40,
   },
   face: {
     fontSize: 15,
     color: '#FFF',
     fontWeight: 'bold',
     textAlign: 'center',
+    
   },
-
   shooterGameContainer: {
+    backgroundColor: "rgb(255, 158, 226)",
     flex: 1,
-    backgroundColor:  "rgb(255, 158, 226)", 
     alignItems: 'center',
-    justifyContent: 'center',
-    height: "120%",
+    justifyContent: 'center', 
+    position: 'relative',
   },
   shooterScoreText: {
-    textAlign: "center" ,
-    position: 'absolute',
-    top: 10,
-    fontSize: 45,
-    color: '#6C5DD3', 
-    textShadowColor: '#8EA7E9', 
+    fontSize: 30,
+    color: '#6C5DD3',
+    textShadowColor: '#8EA7E9',
     textShadowOffset: { width: 1, height: 1 },
     textShadowRadius: 10,
-    marginTop: 40,
-    fontFamily: 'CuteFont_400Regular',
+    fontFamily: 'VT323_400Regular',
   },
   ShooterScreenContainer: {
     flex: 1,
-    paddingBottom: 80, 
-    backgroundColor: 'rgb(255, 158, 226)'
-  },
-  shooterHomeButtonContainer: {
-    position: 'absolute',
-    top: 55,
-    left: 100,
-    zIndex: 10,
+    paddingBottom: 80,
+    backgroundColor: 'rgb(255, 158, 226)',
+    borderWidth: 10, 
+    borderColor: "#6C5DD3",  
+    borderRadius: 50,       
+    overflow: 'hidden', 
   },
   shooterHomeIcon: {
     fontSize: 32,
     color: '#6C5DD3',
   },
+  // Uusi container, johon laitetaan sekä score että home icon
+  headerContainer: {
+    position: 'absolute',
+    top: 30,           // Etäisyys yläreunasta – säädä tarpeen mukaan
+    left: '25%',
+    right: '25%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#76e3df',  // Voit vaihtaa taustavärin mieleiseksesi
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 10,
+    borderWidth: 3,
+    borderColor: "#6C5DD3", 
+    elevation: 4,              // Varjostus
+    zIndex: 100,               // Näkyy muiden elementtien päällä
+  },
 });
-
 
 export default shooterStyles;
